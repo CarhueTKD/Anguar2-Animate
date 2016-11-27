e39aea87e732dd115c82a964d12fc033c3043c3f
carhuetkd <carhuetkd@MacBook-Pro-de-carhuetkd.local>
2016-11-26 21:44:17 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BoxyComponent } from './boxy/boxy.component';
+import { ShrinkOutComponent } from './shrink-out/shrink-out.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PrintComponent } from './print/print.component';
+import { TypeStatusComponent } from './type-status/type-status.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoxyComponent,
+    ShrinkOutComponent,
+    FooterComponent,
+    NavbarComponent,
+    PrintComponent,
+    TypeStatusComponent
   ],
   imports: [
     BrowserModule,
